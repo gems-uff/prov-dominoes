d845a24adc8a229c273e22da2b6eab8ab6483b9d
Victor Alencar <vba.cin@gmail.com>
2019-05-18 00:40:21 -0300

    Inclusão do MoveCommand para recuperação no canvas.

---
+++ Dominoes/src/main/java/command/MoveCommand.java
@@ -0,0 +1,81 @@
+package command;
+
+import boundary.App;
+
+public class MoveCommand extends AbstractCommand {
+
+	private int index;
+	private double oldX;
+	private double oldY;
+	private double y;
+	private double x;
+
+	public MoveCommand() {
+		this.x = -1;
+		this.y = -1;
+	}
+
+	public MoveCommand(int index) {
+		this();
+		this.index = index;
+	}
+
+	public double getOldX() {
+		return oldX;
+	}
+
+	public void setOldX(double oldX) {
+		this.oldX = oldX;
+	}
+
+	public double getOldY() {
+		return oldY;
+	}
+
+	public void setOldY(double oldY) {
+		this.oldY = oldY;
+	}
+
+	public int getIndex() {
+		return index;
+	}
+
+	public void setIndex(int index) {
+		this.index = index;
+	}
+
+	@Override
+	protected String getName() {
+		return MOVE_COMMAND + "(" + index + "," + App.getArea().getData().getDominoes().get(index).getIdRow() + "|"
+				+ App.getArea().getData().getDominoes().get(index).getIdCol() + ")";
+	}
+
+	@Override
+	protected boolean doIt() {
+		if (this.x == -1 && this.y == -1) {
+			this.oldX = App.getArea().getData().getPieces().get(index).getTranslateX();
+			this.oldY = App.getArea().getData().getPieces().get(index).getTranslateY();
+		} else {
+			App.getArea().getData().getPieces().get(index).setTranslateX(x);
+			App.getArea().getData().getPieces().get(index).setTranslateY(y);
+		}
+		return true;
+	}
+
+	@Override
+	protected boolean undoIt() {
+		App.getArea().getData().getPieces().get(index).setTranslateX(oldX);
+		App.getArea().getData().getPieces().get(index).setTranslateY(oldY);
+		return true;
+	}
+
+	public void setX(double translateX) {
+		this.x = translateX;
+
+	}
+
+	public void setY(double translateY) {
+		this.y = translateY;
+	}
+
+}
