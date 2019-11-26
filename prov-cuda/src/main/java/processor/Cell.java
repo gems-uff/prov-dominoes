67f9d4bee8fd3d86e99bb08f9840b12dd1c9f22e
Victor Alencar <vba.cin@gmail.com>
2019-11-26 01:14:22 -0200

    Ajuste de espaçamentos.

--- prov-cuda/src/main/java/processor/Cell.java
+++ prov-cuda/src/main/java/processor/Cell.java
@@ -4,18 +4,18 @@ public class Cell {
 	public int row;
 	public int col;
 	public float value;
-	
-	public Cell(){
-		
+
+	public Cell() {
+
 	}
-	
+
 	public Cell(int row, int col, float value) {
 		super();
 		this.row = row;
 		this.col = col;
 		this.value = value;
 	}
-	
+
 	@Override
 	public String toString() {
 		return "Cell [row=" + row + ", col=" + col + ", value=" + value + "]";
